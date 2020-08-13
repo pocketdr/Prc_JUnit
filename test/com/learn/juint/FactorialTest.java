f6a6792eb464b8f6fefdc1348aaf09d93b8e0a8a
pocketdr <pocketz1783@gmail.com>
2020-08-13 14:23:02 +0800

    change import structure

--- test/com/learn/juint/FactorialTest.java
+++ test/com/learn/juint/FactorialTest.java
@@ -1,9 +1,9 @@
 package com.learn.juint;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 class FactorialTest {
 
